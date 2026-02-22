2ca7a4afc3d4117321332f96d158e4fa7dbd0473
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2026-02-21 20:08:05 -0400

    chore: configura ESLint e Prettier, atualiza .editorconfig e adiciona os arquivos de configuração iniciais

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,58 @@
 import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
     js.configs.recommended,
+    eslintConfigPrettier,
     {
         languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
             globals: {
                 process: 'readonly',
-                console: 'readonly'
+                console: 'readonly',
+                setTimeout: 'readonly',
+                clearTimeout: 'readonly',
+                setInterval: 'readonly',
+                clearInterval: 'readonly',
+                Buffer: 'readonly',
+                URL: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
             },
         },
         rules: {
-            semi: ['off'],
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            'no-debugger': 'error',
+            'no-duplicate-imports': 'error',
+            'no-template-curly-in-string': 'warn',
+            'prefer-const': 'warn',
+            'no-var': 'error',
+            'eqeqeq': ['error', 'always', { null: 'ignore' }],
+            'curly': ['error', 'multi-line'],
+            'no-throw-literal': 'error',
+            'no-return-await': 'warn',
+            'require-await': 'warn',
+        },
+    },
+    {
+        files: ['**/*.test.js', '**/*.spec.js', '**/tests/**/*.js'],
+        languageOptions: {
+            globals: {
+                describe: 'readonly',
+                it: 'readonly',
+                expect: 'readonly',
+                beforeAll: 'readonly',
+                afterAll: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly',
+                jest: 'readonly',
+            },
+        },
+        rules: {
+            'no-unused-vars': 'off',
+            'no-console': 'off',
         },
     },
 ];
