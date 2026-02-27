c04b53412f1e013829492d1fc6a5f3f78307d205
Matheus Batista <matheusifro2020@gmail.com>
2026-02-27 15:29:14 -0400

    chore: implementação da verificação zod ao model de escola

--- src/utils/validators/schemas/zod/SchoolSchema.js
+++ src/utils/validators/schemas/zod/SchoolSchema.js
@@ -1,22 +1,20 @@
 import { z } from 'zod';
 
 const taxIdSchema = z
-  .string('CNPJ é obrigatório.')
+  .string()
+  .min(1, 'CNPJ é obrigatório.')
   .refine((doc) => {
     const replacedDoc = doc.replace(/\D/g, '');
-    return replacedDoc.length >= 11;
-  }, 'CNPJ deve conter no mínimo 11 caracteres.')
+    return replacedDoc.length === 14;
+  }, 'CNPJ deve conter exatamente 14 caracteres numéricos.')
   .refine((doc) => {
     const replacedDoc = doc.replace(/\D/g, '');
-    return replacedDoc.length <= 14;
-  }, 'CNPJ deve conter no máximo 14 caracteres.')
-  .refine((doc) => {
-    const replacedDoc = doc.replace(/\D/g, '');
-    return !!Number(replacedDoc);
+    return /^\d+$/.test(replacedDoc);
   }, 'CNPJ deve conter apenas números.');
 
 const zipCodeSchema = z
   .string()
+  .min(1, 'CEP é obrigatório.')
   .refine((zip) => {
     const replacedZip = zip.replace(/\D/g, '');
     return replacedZip.length === 8;
@@ -27,6 +25,13 @@ const SchoolSchema = z.object({
     .string()
     .min(1, 'Campo nome é obrigatório.')
     .max(100, 'O nome da escola deve ter no máximo 100 caracteres.'),
+  tax_id: taxIdSchema,
+  address: z.object({
+    street: z.string().min(1, 'Campo rua é obrigatório.'),
+    city: z.string().min(1, 'Campo cidade é obrigatório.'),
+    state: z.string().min(1, 'Campo estado é obrigatório.'),
+    zip_code: zipCodeSchema,
+  }),
   active: z.boolean().default(true),
 });
 
