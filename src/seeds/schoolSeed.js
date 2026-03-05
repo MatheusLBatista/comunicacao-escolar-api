da3bc98f91d95a4ca5333183ac07a72d10619958
Matheus Batista <matheusifro2020@gmail.com>
2026-03-04 20:15:35 -0400

    chore: implementação de escola padrão

--- src/seeds/schoolSeed.js
+++ src/seeds/schoolSeed.js
@@ -1,16 +1,25 @@
 import School from '../models/School.js';
 import { fakeMappings } from './globalFakeMapping.js';
-import seedRotas from './rotasSeed.js';
-import seedGrupos from './grupoSeed.js';
 
 export default async function schoolSeed() {
   await School.deleteMany({});
 
-  const rotasCompletas = await seedRotas();
-  const grupos = await seedGrupos(rotasCompletas);
-  const schoolGroup = grupos.find((g) => g.nome === 'School');
-
   const schools = [];
+
+  const defaultSchool = {
+    name: 'Escola Comunica Alunos',
+    tax_id: '12345678000190',
+    address: {
+      street: 'Rua Principal, 100',
+      city: 'São Paulo',
+      state: 'SP',
+      zip_code: '01001000',
+    },
+    active: true,
+  };
+
+  schools.push(defaultSchool);
+
   for (let i = 0; i < 5; i++) {
     schools.push({
       name: fakeMappings.School.name(),
