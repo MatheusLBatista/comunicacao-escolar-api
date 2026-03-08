ab90a22a03642815df960e98478e8ca64743875d
Matheus Batista <matheusifro2020@gmail.com>
2026-03-08 18:31:11 -0400

    test: implementação do teste unitário de SchoolFilterBuilder

--- src/repositories/filters/SchoolFilterBuilder.js
+++ src/repositories/filters/SchoolFilterBuilder.js
@@ -1,4 +1,4 @@
-import School from "../../models/School.js";
+import School from '../../models/School.js';
 
 class SchoolFilterBuilder {
   constructor() {
@@ -8,7 +8,7 @@ class SchoolFilterBuilder {
 
   withName(name) {
     if (name) {
-      this.filters.name = { $regex: name, $options: "i" };
+      this.filters.name = { $regex: name, $options: 'i' };
     }
 
     return this;
@@ -16,15 +16,17 @@ class SchoolFilterBuilder {
 
   withTaxId(tax_id) {
     if (tax_id) {
-      this.filters.tax_id = { $regex: tax_id, $options: "i" };
+      this.filters.tax_id = { $regex: tax_id, $options: 'i' };
     }
 
     return this;
   }
 
   withActive(active) {
-    if (active !== undefined) {
-      this.filters.active = active === "true" || active === true;
+    if (active === 'true' || active === true) {
+      this.filters.active = true;
+    } else if (active === 'false' || active === false) {
+      this.filters.active = false;
     }
 
     return this;
@@ -32,7 +34,7 @@ class SchoolFilterBuilder {
 
   withCity(city) {
     if (city) {
-      this.filters["address.city"] = { $regex: city, $options: "i" };
+      this.filters['address.city'] = { $regex: city, $options: 'i' };
     }
 
     return this;
@@ -40,7 +42,7 @@ class SchoolFilterBuilder {
 
   withState(state) {
     if (state) {
-      this.filters["address.state"] = { $regex: state, $options: "i" };
+      this.filters['address.state'] = { $regex: state, $options: 'i' };
     }
 
     return this;
@@ -48,7 +50,7 @@ class SchoolFilterBuilder {
 
   withZipCode(zip_code) {
     if (zip_code) {
-      this.filters["address.zip_code"] = { $regex: zip_code, $options: "i" };
+      this.filters['address.zip_code'] = { $regex: zip_code, $options: 'i' };
     }
 
     return this;
@@ -57,11 +59,11 @@ class SchoolFilterBuilder {
   withAddress(address) {
     if (address) {
       this.filters.$or = [
-        { "address.street": { $regex: address, $options: "i" } },
-        { "address.number": { $regex: address, $options: "i" } },
-        { "address.city": { $regex: address, $options: "i" } },
-        { "address.state": { $regex: address, $options: "i" } },
-        { "address.zip_code": { $regex: address, $options: "i" } }
+        { 'address.street': { $regex: address, $options: 'i' } },
+        { 'address.number': { $regex: address, $options: 'i' } },
+        { 'address.city': { $regex: address, $options: 'i' } },
+        { 'address.state': { $regex: address, $options: 'i' } },
+        { 'address.zip_code': { $regex: address, $options: 'i' } },
       ];
     }
 
