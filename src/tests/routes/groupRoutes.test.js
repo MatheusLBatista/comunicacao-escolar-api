c03ba0c98acd632e617261e8e55a88aa6d6b8645
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2026-04-08 09:19:06 -0400

    test: renomeia arquivo de grupo para group

---
+++ src/tests/routes/groupRoutes.test.js
@@ -0,0 +1,108 @@
+import request from 'supertest';
+import { describe, it, expect, beforeAll } from '@jest/globals';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.INTEGRATION_BASE_URL || `http://localhost:${PORT}`;
+
+describe('Grupos', () => {
+  let tokenAdmin;
+  let tokenUser;
+  let idGrupo;
+  beforeAll(async () => {
+    const res = await request(BASE_URL)
+      .post('/login')
+      .send({ email: 'admin@admin.com', password: 'Senha@123' });
+    tokenAdmin = res.body?.data?.user?.access_token;
+  });
+  it('Deve listar os grupos com sucesso sendo administrador e verificar se todos os campos estão sendo informados', async () => {
+    const res = await request(BASE_URL)
+      .get('/grupos')
+      .set('Authorization', `Bearer ${tokenAdmin}`)
+      .expect(200);
+    expect(Array.isArray(res.body?.data?.docs)).toBe(true);
+    expect(res.body?.data?.docs[0]).toHaveProperty('_id');
+    expect(res.body?.data?.docs[0]).toHaveProperty('nome');
+    expect(res.body?.data?.docs[0]).toHaveProperty('descricao');
+    expect(res.body?.data?.docs[0]).toHaveProperty('ativo');
+    expect(res.body?.data?.docs[0]).toHaveProperty('permissions');
+    expect(res.body?.message).toEqual('Requisição bem-sucedida');
+    idGrupo = res.body?.data?.docs[0]._id;
+  });
+  it('Deve listar grupo por id', async () => {
+    const res = await request(BASE_URL)
+      .get(`/grupos/${idGrupo}`)
+      .set('Authorization', `Bearer ${tokenAdmin}`)
+      .expect(200);
+    expect(res.body?.data?._id).toEqual(idGrupo);
+    expect(res.body?.data).toHaveProperty('_id');
+    expect(res.body?.data).toHaveProperty('nome');
+    expect(res.body?.data).toHaveProperty('descricao');
+    expect(res.body?.data).toHaveProperty('ativo');
+    expect(res.body?.data).toHaveProperty('permissions');
+    expect(res.body?.message).toEqual('Requisição bem-sucedida');
+  });
+  it('Deve listar grupos pelo nome, usando BasicUser', async () => {
+    const res = await request(BASE_URL)
+      .get('/grupos')
+      .set('Authorization', `Bearer ${tokenAdmin}`)
+      .query({ nome: 'BasicUser' })
+      .expect(200);
+    expect(res.body?.data?.docs[0]).toHaveProperty('nome');
+    expect(res.body?.data?.docs[0]).toHaveProperty('_id');
+    expect(res.body?.data?.docs[0]).toHaveProperty('ativo');
+    expect(res.body?.data?.docs[0]).toHaveProperty('permissions');
+    expect(res.body?.data?.docs[0]).toHaveProperty('descricao');
+    expect(res.body?.data?.docs).toHaveLength(1);
+    expect(res.body?.data?.docs[0].nome).toEqual('BasicUser');
+  });
+  it('Deve listar grupos pela descricação, usando "Grupo com acesso total a todas as rotas"', async () => {
+    const res = await request(BASE_URL)
+      .get('/grupos')
+      .set('Authorization', `Bearer ${tokenAdmin}`)
+      .query({ descricao: 'Grupo com acesso total a todas as rotas' })
+      .expect(200);
+    expect(res.body?.data?.docs[0]).toHaveProperty('nome');
+    expect(res.body?.data?.docs[0]).toHaveProperty('_id');
+    expect(res.body?.data?.docs[0]).toHaveProperty('ativo');
+    expect(res.body?.data?.docs[0]).toHaveProperty('permissions');
+    expect(res.body?.data?.docs[0]).toHaveProperty('descricao');
+    expect(res.body?.data?.docs[0].descricao).toEqual(
+      'Grupo com acesso total a todas as rotas',
+    );
+    expect(res.body?.data?.docs).toHaveLength(1);
+  });
+  it('Deve listar grupos pela campo "ativo", usando "true"', async () => {
+    const res = await request(BASE_URL)
+      .get('/grupos')
+      .set('Authorization', `Bearer ${tokenAdmin}`)
+      .query({ ativo: true })
+      .expect(200);
+    expect(res.body?.data?.docs[0]).toHaveProperty('nome');
+    expect(res.body?.data?.docs[0]).toHaveProperty('_id');
+    expect(res.body?.data?.docs[0]).toHaveProperty('ativo');
+    expect(res.body?.data?.docs[0]).toHaveProperty('permissions');
+    expect(res.body?.data?.docs[0]).toHaveProperty('descricao');
+    expect(res.body?.data?.docs[0].ativo).toEqual(true);
+  });
+  it('Deve falhar ao tentar listar grupos não sendo um administrador', async () => {
+    const login = await logar(
+      process.env.TEACHER_EMAIL || 'maria.teacher@escola.com',
+      process.env.TEACHER_PASSWORD || 'Senha@123',
+    );
+    tokenUser = login.body?.data?.user?.access_token;
+    const res = await request(BASE_URL)
+      .get('/grupos')
+      .set('Authorization', `Bearer ${tokenUser}`);
+
+    expect([403, 498]).toContain(res.status);
+  });
+});
+async function logar(email, password) {
+  const res = await request(BASE_URL)
+    .post('/login')
+    .send({ email: email, password: password });
+  return res;
+}
